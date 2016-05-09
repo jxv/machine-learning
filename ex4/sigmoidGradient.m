bb7b41736c66d6f49f5699dd494db9dd519221be
jvargas <jvargas@cj.com>
2016-05-09 11:56:55 -0700

    ex4

--- ex4/sigmoidGradient.m
+++ ex4/sigmoidGradient.m
@@ -12,22 +12,9 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
-
-
-
-
-
-
-
-
-
-
-
+sigmoid = sigmoid(z);
+g = sigmoid .* (1 .- sigmoid);
 
 % =============================================================
-
-
-
 
 end
